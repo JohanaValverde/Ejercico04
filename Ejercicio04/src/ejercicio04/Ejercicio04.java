06b6849444c49f0eefdb39699d2d95676de46b53
adrian159 <Danner.rengifo@misena.edu.co>
2016-02-24 20:38:58 -0500

    Commit Ejercicio04

---
+++ Ejercicio04/src/ejercicio04/Ejercicio04.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio04;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Ejercicio04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Hola Mundo");
+
+        int a, b;
+        a = 5;
+        b = 10;
+        System.out.println("a+b = " + (a + b));
+
+        System.out.println("a-b = " + (a - b));
+
+        System.out.println("a/b = " + (a / b));
+
+        System.out.println("a*b = " + (a * b));
+    }
+    }
+    
+
